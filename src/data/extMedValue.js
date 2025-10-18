557f8dca58e9c41ff20fd61aa7651f370dc475a0
makizo <k.ikeda.ka2@gmail.com>
2025-10-18 14:35:42 +0900

    extMedValue: add yomigana

--- src/data/extMedValue.js
+++ src/data/extMedValue.js
@@ -1,10 +1,12 @@
 export const extMedValue = [
   { 
     medName: 'クレナフィン',
-    contentValue: '3.56g' 
+    contentValue: '3.56g', 
+    yomi: 'kurenafin'
   },
   { 
     medName: 'ナゾネックス56噴霧', 
-    contentValue: '10g' 
+    contentValue: '10g',
+    yomi: 'nazonekkusu'
   }
 ];
