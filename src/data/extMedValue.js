4ff9f4cfa4057b465356308c413ae60ea41a846c
K.Ikeda(Senn) <k.ikeda.ka2@gmail.com>
2025-10-29 17:35:06 +0900

    extMedValue: add new items

--- src/data/extMedValue.js
+++ src/data/extMedValue.js
@@ -4,6 +4,26 @@ export const extMedValue = [
     contentValue: '3.56g', 
     yomi: 'kurenafin'
   },
+  {
+    medName: 'クラビット点眼0.5％',
+    contentValue: '5ml',
+    yomi: 'kurabitto'
+  },
+  {
+    medName: 'クラビット点眼1.5％',
+    contentValue: '5ml',
+    yomi: 'kurabitto'
+  },
+  {
+    medName: 'ボルタレンゲル1％ 25g',
+    contentValue: '25g',
+    yomi: 'borutaren'
+  },
+  {
+    medName: 'ボルタレンゲル1％ 50g',
+    contentValue: '50g',
+    yomi: 'borutaren'
+  },
   { 
     medName: 'ナゾネックス56噴霧', 
     contentValue: '10g',
